5515eb1d3409a5376bd222a96ae0fc080369c4eb
IgorSpi <igor13pao@hotmail.com>
2018-03-28 15:07:33 +0300

    first igor commit

---
+++ src/ArgsParser.java
@@ -0,0 +1,50 @@
+public class ArgsParser
+{
+    private int maxLength;
+    private String[] parsedArgs;
+    private String[] options;
+    private String[] foundArguments;
+
+
+    ArgsParser(String[] args)
+    {
+        maxLength=args.length;
+        parsedArgs=new String[maxLength];
+        parsedArgs=args;
+
+        detectOptions();
+        detectArguments();
+        System.out.println("options are:" + options[0] + " " + options[1]);
+        System.out.println("arguments are:" + foundArguments[0] + " " + foundArguments[1]);
+    }
+    public void argsPrinter()
+    {
+        for(int i=0;i<maxLength;i++)
+            System.out.println(parsedArgs[i]);
+    }
+    private void detectOptions()
+    {
+        options = new String[maxLength];
+        int numOfOpts=0;
+        for (int i=0;i<maxLength;i++)
+            if(parsedArgs[i].startsWith("-"))
+            {
+                options[numOfOpts]=parsedArgs[i];
+                numOfOpts++;
+            }
+    }
+    private void detectArguments()
+    {
+        foundArguments = new String[maxLength];
+        int numOfOpts=0;
+        for (int i=0;i<maxLength;i++)
+            if(!parsedArgs[i].startsWith("-"))
+            {
+                foundArguments[numOfOpts]=parsedArgs[i];
+                numOfOpts++;
+            }
+    }
+    public static void main(String[] args)
+    {
+    }
+}
